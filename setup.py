68882a788d44632cc0cf80d0a5c0a935f200168d
Phil Schleihauf <uniphil@gmail.com>
2012-08-06 13:24:11 -0400

    Fix distribution -- include the readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='HumanFuture',
-    version='0.1.2',
+    version='0.1.3',
     author='Philip Schleihauf and Jamie Macdonald',
     author_email='uniphil@gmail.com',
     license='GPLv3',
